96af1137114831860899d0ae506c7b9013b358d1
joesobo <soboleskijoe@gmail.com>
2025-10-14 18:52:29 -0700

    remove networking signals

--- scripts/steamworks.gd
+++ scripts/steamworks.gd
@@ -8,17 +8,5 @@ func _ready() -> void:
 
 	Steam.allowP2PPacketRelay(true)
 
-	Steam.network_messages_session_request.connect(_on_network_messages_session_request)
-	Steam.network_messages_session_failed.connect(_on_network_messages_session_failed)
-
 func _process(_delta: float) -> void:
 	Steam.run_callbacks()
-
-# Accept incoming Network Messages session requests
-func _on_network_messages_session_request(remote_steam_id: int) -> void:
-	print("Network Messages session request from: ", remote_steam_id)
-	Steam.acceptSessionWithUser(remote_steam_id)
-
-# Log session failures for debugging
-func _on_network_messages_session_failed(_reason: int, _remote_steam_id: int, _connection_state: int, debug_message: String) -> void:
-	print("Network Messages session failed with: ", debug_message)
