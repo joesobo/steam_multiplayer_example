56b5a72acc7f8a4dfee6507547f044b2cd2ae7f6
joesobo <soboleskijoe@gmail.com>
2025-10-14 18:22:47 -0700

    test debug

--- scripts/lobby.gd
+++ scripts/lobby.gd
@@ -32,6 +32,7 @@ func try_register_player() -> void:
 
 		if multiplayer.multiplayer_peer and multiplayer.multiplayer_peer.get_connection_status() == MultiplayerPeer.CONNECTION_CONNECTED:
 			if !multiplayer.is_server():
+				print("CLIENT: Disabling button, starting sync")
 				ready_button.disabled = true
 				ready_button.text = "Syncing..."
 
@@ -41,10 +42,13 @@ func try_register_player() -> void:
 			_player_ready[my_id] = false
 
 			# Broadcast our info to everyone
+			print("Calling _rpc_register_player for ", my_id)
 			_rpc_register_player.rpc(my_id, Steam.getPersonaName(), false)
 
 			if !multiplayer.is_server():
+				print("CLIENT: Waiting for player_list_updated signal...")
 				await player_list_updated
+				print("CLIENT: Signal received! Re-enabling button")
 				ready_button.disabled = false
 				ready_button.text = "Ready"
 			else:
@@ -93,6 +97,7 @@ func _update_player_list() -> void:
 
 		players_list.add_child(container)
 
+	print("Emitting player_list_updated signal. Player count: ", _player_names.size())
 	player_list_updated.emit()
 
 func _on_ready_pressed() -> void:
